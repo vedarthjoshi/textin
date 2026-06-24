730bc8690b49d26df96c6d1ed4293d3fdb45b4c5
Vedarth Joshi <121310165+vedarthjoshi@users.noreply.github.com>
2026-06-24 16:08:31 +0530

    Add Clerk webhook integration for user events

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ import { clerkMiddleware } from "@clerk/express";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js"
 
 //ini
 const app = express();
@@ -19,6 +20,9 @@ const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+//WebHooks
+app.use("/api/webhooks/clerk", express.raw({type:"application/json"}),clerkWebhook)
 
 // Middlewares
 app.use(express.json());
